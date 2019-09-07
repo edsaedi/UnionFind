14ac6b8b0064928a42b38b1fd4be5b41713f6cb1
Edan Saedi <edsaedi@gmail.com>
2019-09-06 17:40:33 -0700

    I learned Union Find.

---
+++ UnionFind/UnionFind/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace UnionFind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] file = File.ReadAllLines("file.txt");
+
+            int numObjects = int.Parse(file[0]);
+
+            UnionFind uf = new UnionFind(numObjects);
+
+            for (int i = 1; i < file.Length; i++)
+            {
+                string temp = file[i];
+
+                string[] split = temp.Split(',');
+
+                int p = int.Parse(split[0]);
+                int q = int.Parse(split[1]);
+
+                if (!uf.Connected(p, q))
+                {
+                    uf.Union(p, q);
+                    Console.WriteLine($"{p} {q}");
+                }
+            }
+
+            Console.WriteLine(uf.NumSets);
+        }
+    }
+}
---
+++ UnionFind/UnionFind/UnionFind.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace UnionFind
+{
+    public class UnionFind
+    {
+        int[] id;
+        public UnionFind(int capacity)
+        {
+            id = new int[capacity];
+
+            for (int i = 0; i < id.Length; i++)
+            {
+                id[i] = i;
+            }
+        }
+
+        public int NumSets => id.Distinct().Count();
+
+        public bool Connected(int p, int q) => Find(p) == Find(q);
+
+        public int Find(int q) => id[q];
+
+        public void Union(int p, int q)
+        {
+            int pId = Find(p);
+
+            int qId = Find(q);
+
+            //If connected
+            if (pId == qId) return;
+
+            for (int i = 0; i < id.Length; i++)
+            {
+                if (id[i] == pId)
+                {
+                    id[i] = qId;
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
