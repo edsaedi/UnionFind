842e6b04a44c175f2682fdab0259b4d3eb2443dd
Edan Saedi <edsaedi@gmail.com>
2019-09-11 17:38:22 -0700

    I continued union find

--- UnionFind/UnionFind/Program.cs
+++ UnionFind/UnionFind/Program.cs
@@ -7,29 +7,30 @@ namespace UnionFind
     {
         static void Main(string[] args)
         {
+
             string[] file = File.ReadAllLines("file.txt");
-
-            int numObjects = int.Parse(file[0]);
-
-            UnionFind uf = new UnionFind(numObjects);
+            UnionFindPractice Union = new UnionFindPractice(int.Parse(file[0]));
 
             for (int i = 1; i < file.Length; i++)
             {
-                string temp = file[i];
-
-                string[] split = temp.Split(',');
-
-                int p = int.Parse(split[0]);
-                int q = int.Parse(split[1]);
-
-                if (!uf.Connected(p, q))
-                {
-                    uf.Union(p, q);
-                    Console.WriteLine($"{p} {q}");
-                }
+                string[] ids = file[1].Split(',');
+                Union.Union(int.Parse(ids[0]), int.Parse(ids[1]));
             }
 
-            Console.WriteLine(uf.NumSets);
+            Console.WriteLine(Union.NumSets());
+            Console.WriteLine();
+
+
+
+            // Questions:
+            // Is phoebe friends with rachel?
+            // Is jim friends with creed?
+            // Is michael friends with pam?
+            // is chandler friends with creed?
+            // how many friend groups are there?
+            // followup, list each person in the friend groups
+            // who is in the largest friend group
+            // who is in the smallest friend group
         }
     }
 }
---
+++ UnionFind/UnionFind/UnionFindPractice.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnionFind
+{
+    public class UnionFindPractice<T> where T : IEquatable<T>
+    {
+        Dictionary<T, int> map;
+        T[] id;
+
+        public UnionFindPractice(List<T> list, int capacity)
+        {
+            id = new T[capacity];
+            map = new Dictionary<T, int>(capacity);
+            for (int i = 0; i < list.Count; i++)
+            {
+                map.Add(list[i], i);
+            }
+        }
+
+        public int NumSets() => id.Distinct().Count();
+
+        public IEnumerable<T> TSets()
+        {
+            return id.Distinct().ToList();
+        }
+
+        public bool IsConnected(T p, T q) => Find(p).Equals(Find(q));
+
+        public void Union(T p, T q)
+        {
+            if (IsConnected(p, q))
+            {
+                return;
+            }
+
+            var pId = Find(p);
+            var qId = Find(q);
+
+            for (int i = 0; i < id.Length; i++)
+            {
+                if (id[i].Equals(pId))
+                {
+                    id[i] = qId;
+                }
+            }
+        }
+
+        public T Find(T item)
+        {
+            return id[map[item]];
+        }
+
+    }
+}
