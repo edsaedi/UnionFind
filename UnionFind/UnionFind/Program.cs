d7ea86632ed83e3b1b2837347e36dbcbb3797828
Edan Saedi <edsaedi@gmail.com>
2019-09-20 17:40:30 -0700

    I finished Quick Union. Working on weighted/

--- UnionFind/UnionFind/Program.cs
+++ UnionFind/UnionFind/Program.cs
@@ -8,39 +8,53 @@ namespace UnionFind
     {
         static void Main(string[] args)
         {
+            QuickUnion quickUnion = new QuickUnion(10);
+            quickUnion.Union(0, 2);
+            quickUnion.Union(4, 5);
+            quickUnion.Union(4, 6);
+            quickUnion.Union(7, 6);
+            quickUnion.Union(7, 9);
+            quickUnion.Union(8, 3);
 
-            string[] file = File.ReadAllLines(@"\\GMRDC1\Folder Redirection\Edan.Saedi\Desktop\UnionFind\UnionFind\UnionFind\input.txt");
-            List<string> list = new List<string>();
-            int capacity = int.Parse(file[0]);
-            for (int i = 1; i < capacity + 1; i++)
+            for (int i = 0; i < 10; i++)
             {
-                list.Add(file[i]);
+                Console.Write(quickUnion.id[i]);
+                Console.WriteLine($", {quickUnion.Find(i)}");
             }
 
-            UnionFindPractice<string> Union = new UnionFindPractice<string>(list);
+            
+            //string[] file = File.ReadAllLines(@"\\GMRDC1\Folder Redirection\Edan.Saedi\Desktop\UnionFind\UnionFind\UnionFind\input.txt");
+            //List<string> list = new List<string>();
+            //int capacity = int.Parse(file[0]);
+            //for (int i = 1; i < capacity + 1; i++)
+            //{
+            //    list.Add(file[i]);
+            //}
 
-            for (int i = capacity + 2; i < file.Length; i++)
-            {
-                string[] ids = file[i].Split(',');
-                Union.Union(ids[0].Trim(), ids[1].Trim());
-            }
+            //UnionFindPractice<string> Union = new UnionFindPractice<string>(list);
+
+            //for (int i = capacity + 2; i < file.Length; i++)
+            //{
+            //    string[] ids = file[i].Split(',');
+            //    Union.Union(ids[0].Trim(), ids[1].Trim());
+            //}
 
 
-            //Questions:
-            //Is phoebe friends with rachel?
-            Console.WriteLine($"Is phoebe friends with rachel: {Union.IsConnected("Phoebe", "Rachael")}");
-            // Is jim friends with creed?
-            Console.WriteLine($"Is Jim friends with Creed: {Union.IsConnected("Jim", "Creed")}");
-            // Is michael friends with pam?
-            Console.WriteLine($"Is Michael friends with Pam: {Union.IsConnected("Michael", "Pam")}");
-            // is chandler friends with creed?
-            Console.WriteLine($"Is Chandler friends with Creed: {Union.IsConnected("Chandler", "Creed")}");
-            // how many friend groups are there?
-            Console.WriteLine($"How many friend groups are there? : {Union.NumSets()}");
-            // followup, list each person in the friend groups
-            //Console.WriteLine(Union.GetAllGroupsWithMembers());
-            //who is in the largest friend group
-            //who is in the smallest friend group
+            ////Questions:
+            ////Is phoebe friends with rachel?
+            //Console.WriteLine($"Is phoebe friends with rachel: {Union.IsConnected("Phoebe", "Rachael")}");
+            //// Is jim friends with creed?
+            //Console.WriteLine($"Is Jim friends with Creed: {Union.IsConnected("Jim", "Creed")}");
+            //// Is michael friends with pam?
+            //Console.WriteLine($"Is Michael friends with Pam: {Union.IsConnected("Michael", "Pam")}");
+            //// is chandler friends with creed?
+            //Console.WriteLine($"Is Chandler friends with Creed: {Union.IsConnected("Chandler", "Creed")}");
+            //// how many friend groups are there?
+            //Console.WriteLine($"How many friend groups are there? : {Union.NumSets()}");
+            //// followup, list each person in the friend groups
+            ////Console.WriteLine(Union.GetAllGroupsWithMembers());
+            ////who is in the largest friend group
+            ////who is in the smallest friend group
         }
     }
 }
---
+++ UnionFind/UnionFind/QuickUnion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace UnionFind
+{
+    public class QuickUnion
+    {
+        public int[] id { get; private set; }
+        public int[] size { get; private set; }
+
+        public int Capacity { get; private set; }
+
+        public QuickUnion(int capacity)
+        {
+            Capacity = capacity;
+            id = new int[capacity];
+            size = new int[capacity];
+
+            for (int i = 0; i < capacity; i++)
+            {
+                id[i] = i;
+                size[i] = 1;
+            }
+
+        }
+
+        public int Find(int index)
+        {
+            while (id[index] != index)
+            {
+                index = id[index];
+            }
+            return id[index];
+        }
+
+        public bool IsConnected(int p, int q) => Find(p) == Find(q);
+
+        public void Union(int p, int q)
+        {
+            if (size[p] < size[q])
+            {
+                id[p] = id[q];
+                size[p] += size[q];
+            }
+            else
+            {
+                id[q] = id[p];
+                size[q] += size[p];
+            }
+
+        }
+    }
+}
